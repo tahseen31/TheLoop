3ac65e43cb39a48b757caece3719d67e09fcde09
Archanajith <ajith.a@northeastern.edu>
2021-08-02 20:30:08 -0400

    Basic Get Started and Login Activity

--- app/src/main/java/edu/neu/madcourse/theloop/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/theloop/MainActivity.java
@@ -2,13 +2,39 @@ package edu.neu.madcourse.theloop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button getStartedButton;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStartedButton = findViewById(R.id.button_get_started);
+        getStartedButton.setOnClickListener(this);
+        loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_get_started:
+                Intent intent_get_started = new Intent(MainActivity.this, GetStartedScrollActivity.class);
+                startActivity(intent_get_started);
+                break;
+            case R.id.button_login:
+                Intent intent_login = new Intent(MainActivity.this, MainLoginActivity.class);
+                startActivity(intent_login);
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/edu/neu/madcourse/theloop/MainLoginActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.theloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
