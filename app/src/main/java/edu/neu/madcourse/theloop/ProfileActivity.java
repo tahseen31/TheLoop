66e5c5136fb2f475b576ba0db10c4546986e38e3
tahseen31 <63615375+tahseen31@users.noreply.github.com>
2021-08-11 18:14:03 -0400

    Log out

--- app/src/main/java/edu/neu/madcourse/theloop/ProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/theloop/ProfileActivity.java
@@ -2,13 +2,30 @@ package edu.neu.madcourse.theloop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logOut = (Button) findViewById(R.id.logout);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
 }
