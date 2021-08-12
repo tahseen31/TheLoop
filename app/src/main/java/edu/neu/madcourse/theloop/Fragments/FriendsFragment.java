efa8bbfb5be8bb26a9f2bbabcc2683a7e7662d35
tahseen31 <63615375+tahseen31@users.noreply.github.com>
2021-08-12 03:38:48 -0400

    Chat fragments added

---
+++ app/src/main/java/edu/neu/madcourse/theloop/Fragments/FriendsFragment.java
@@ -0,0 +1,35 @@
+package edu.neu.madcourse.theloop.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.neu.madcourse.theloop.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FriendsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendsFragment extends Fragment {
+
+
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/theloop/ProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/theloop/ProfileActivity.java
@@ -1,30 +1,78 @@
 package edu.neu.madcourse.theloop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
-    private Button logOut;
+
+
+    FirebaseUser firebaseUser;
+    DatabaseReference myRef;
+    private Button connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        logOut = (Button) findViewById(R.id.logout);
-
-        logOut.setOnClickListener(new View.OnClickListener() {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        myRef = FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                Toast.makeText(ProfileActivity.this, "User Login: " + user.getFullName(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
+
+        // Connect with Friends
+        connect = findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileActivity.this, chatActivity.class));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
