5a10734474cdd46b8d30ee9d35b2d6408e5847ca
tahseen31 <63615375+tahseen31@users.noreply.github.com>
2021-08-12 22:32:46 -0400

    get all users in chat activity

---
+++ app/src/main/java/edu/neu/madcourse/theloop/Adapters/FriendsAdapter.java
@@ -0,0 +1,63 @@
+package edu.neu.madcourse.theloop.Adapters;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.neu.madcourse.theloop.R;
+import edu.neu.madcourse.theloop.User;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<User> friends;
+
+    public FriendsAdapter(@NonNull  Context context, List<User> friends) {
+        this.context = context;
+        this.friends = friends;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.friend_item, parent, false);
+
+        return new FriendsAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  FriendsAdapter.ViewHolder holder, int position) {
+        User users = friends.get(position);
+        holder.username.setText(users.getFullName());
+
+        // set Image
+        holder.image.setImageResource(R.mipmap.ic_launcher);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return friends.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView username;
+        public ImageView image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.textView3);
+            image = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
--- app/src/main/java/edu/neu/madcourse/theloop/Fragments/FriendsFragment.java
+++ app/src/main/java/edu/neu/madcourse/theloop/Fragments/FriendsFragment.java
@@ -2,20 +2,36 @@ package edu.neu.madcourse.theloop.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.neu.madcourse.theloop.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FriendsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.madcourse.theloop.Adapters.FriendsAdapter;
+import edu.neu.madcourse.theloop.R;
+import edu.neu.madcourse.theloop.User;
+
+
 public class FriendsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private FriendsAdapter friendsAdapter;
+    private List<User> mUsers;
 
 
 
@@ -29,7 +45,42 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        ReadUsers();
+        return view;
     }
+
+    private void ReadUsers() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren() ) {
+                    User user = snapshot.getValue(User.class);
+                    assert user != null;
+                    if( !user.getEmail().equals(firebaseUser.getEmail())){
+                        mUsers.add(user);
+                    }
+
+                    friendsAdapter = new FriendsAdapter(getContext(), mUsers);
+                    recyclerView.setAdapter(friendsAdapter);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError error) {
+
+            }
+        });
+    }
+
 }
